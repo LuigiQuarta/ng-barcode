9aa3c69475f4a389f693e1184ed973421f931a3d
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2015-11-28 01:44:28 +0100

    Set color preference

--- demo/app/demo.js
+++ demo/app/demo.js
@@ -10,8 +10,8 @@
     function DemoCtrl(){
         this.textField = 'Hello world';
         this.barcodeInput = this.barcodeInput || this.textField;
-        this.colorBarcode = '#004E71';
-        this.colorBackground = [200, 200, 200];
+        this.colorBarcode = '#0288D1';
+        this.colorBackground = [255, 255, 255];
         this.updateBarcode = updateBarcode;
 
         function updateBarcode(){
