124baa1f575e36ffa7b6fd6c16523cb5ada1a730
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2015-11-28 01:14:34 +0100

    Color preference for Code39

--- demo/app/demo.js
+++ demo/app/demo.js
@@ -10,6 +10,8 @@
     function DemoCtrl(){
         this.textField = 'Hello world';
         this.barcodeInput = this.barcodeInput || this.textField;
+        this.colorBarcode = '#004E71';
+        this.colorBackground = [200, 200, 200];
         this.updateBarcode = updateBarcode;
 
         function updateBarcode(){
